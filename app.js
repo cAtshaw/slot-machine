c72ff647343b8536f672e083b5adf72e01c7c68e
cAtshaw <swiftj101@hotmail.com>
2023-05-30 16:14:27 +0100

    Added winnings functions.

--- app.js
+++ app.js
@@ -125,9 +125,33 @@ const printRows = (rows) => {
     }
 }
 
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0
+
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row]
+        let allSame = true
+
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false
+                break
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBOL_VALUE[symbols[0]]
+        }
+    }
+
+    return winnings
+}
+
 let balance = deposit()
 const numberOfLines = getNumberOfLines()
 const bet = getBet(balance)
 const reels = spin()
 const rows = transpose(reels)
 printRows(rows)
+const winnings = getWinnings(rows, bet, numberOfLines)
+console.log("Youn won, $" + winnings.toString())
