3b231f26b700912cf7f336fa564a71ae400482bc
cAtshaw <swiftj101@hotmail.com>
2023-05-30 15:06:30 +0100

    Deposit, Number of lines and bet amount functions.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// 1. Deposit some money.
+// 1. Deposit some money. YUP~~~
 // 2. Determine number of lines to bet on.
-// 3. Colect a bet amount for slot machine.
+// 3. Collect a bet amount for slot machine.
 // 4. Spin the slot machine.
 // 5. Check if the user won
 // 6. Give the user their winnings
@@ -9,13 +9,65 @@
 
 const prompt = require("prompt-sync")();
 
-const deposit = () => {
-    const depositAmount = prompt("Enter a deposit amount: ")
-    const numberDepositAmount = parseFloat(depositAmount)
 
-    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-        console.log("Invalid deposit amount, try again.")
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+
+}
+
+
+
+
+
+
+
+
+const deposit = () => {
+    while (true) {
+
+        const depositAmount = prompt("Enter a deposit amount: ")
+        const numberDepositAmount = parseFloat(depositAmount)
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, try again.")
+        } else {
+            return numberDepositAmount
+        }
     }
 }
 
-deposit();
+const getNumberOfLines = () => {
+    while (true) {
+
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")
+        const numberOfLines = parseFloat(lines)
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.")
+        } else {
+            return numberOfLines
+        }
+    }
+}
+
+
+const getBet = (balance, lines) => {
+    while (true) {
+
+        const bet = prompt("Enter the bet per line: ")
+        const numberBet = parseFloat(bet)
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > (balance / lines)) {
+            console.log("Invalid bet, try again.")
+        } else {
+            return numberBet
+        }
+    }
+}
+
+let balance = deposit()
+const numberOfLines = getNumberOfLines()
+const bet = getBet(balance)
