35d0d12b5d831f81014cc2a6b74cd9adc5001f8e
cAtshaw <swiftj101@hotmail.com>
2023-05-30 15:29:36 +0100

    Created Spin function & reel arrays

--- app.js
+++ app.js
@@ -78,8 +78,25 @@ const getBet = (balance, lines) => {
 
 const spin = () => {
     const symbols = []
-    for (symbol of Object.entries(SYMBOL_COUNT))
+    for (const [symbol, count] of Object.entries(SYMBOL_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol)
+        }
+    }
+
+    const reels = [[], [], []]
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols]
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex]
+            reels[i].push(selectedSymbol)
+            reelSymbols.splice(randomIndex, 1)
+        }
+    }
 }
+
+spin()
 
 
 let balance = deposit()
